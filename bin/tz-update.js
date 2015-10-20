501e5f96615c3c03b6ec0cfc6ee2e38b09324f12
Ricardo Engel <ricengel@mookt.org>
2015-10-20 23:45:02 +0300

    Added Linux (Ubuntu) support

--- bin/tz-update.js
+++ bin/tz-update.js
@@ -2,7 +2,14 @@
 
 var log = require('verbalize');
 var request = require('request-promise');
-var tz = require('../lib/tz-update-windows.js');
+if (process.platform == 'linux')
+    var tz = require('../lib/tz-update-linux.js');
+else if (process.platform == 'win32')
+    var tz = require('../lib/tz-update-windows.js');
+else {
+    log.error('Unsupported OS');
+    process.exit(1);
+}
 var new_tz;
 
 request({uri: 'http://freegeoip.net/json', json: true})
@@ -11,7 +18,7 @@ request({uri: 'http://freegeoip.net/json', json: true})
   return tz.getTimezone();
 })
 .then(res => {
-  var current_tz = res.stdout;
+  var current_tz = res.stdout.trim();
   if (res.stdout != new_tz) {
     log.info("Previously: " + current_tz);
     return tz.setTimezone(new_tz);
@@ -23,7 +30,7 @@ request({uri: 'http://freegeoip.net/json', json: true})
   }  
 })
 .then(res => {
-  var current_tz = res.stdout;
+  var current_tz = res.stdout.trim();
   log.info("Now set to: " + current_tz);
 })
 .catch(err => {
